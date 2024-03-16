51bbaf8e10ff048bf7ef9cbbe0bc17cb06305833
kyujenius <hkj020607@gmail.com>
2024-03-16 16:08:29 +0900

    setting TDD with patching

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x,y):
+    """x랑 y를 더하고 결과를 나타내준다."""
+    return x+y
+
+
+def sub(x,y):
+    """x랑 y를 뺀다."""
+    return x-y
+    
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """calc 모듈 테스트"""
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        """성공"""
+        self.assertEqual(res,11)
+
+    def test_sub_numbers(self):
+        res = calc.sub(10, 15)
+        """실패, -5가 맞다."""
+        self.assertEqual(res,5)
