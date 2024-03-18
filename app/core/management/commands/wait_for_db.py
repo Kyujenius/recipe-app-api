951dbdad77779fca1666e2ce922a8fdd0ea13b22
kyujenius <hkj020607@gmail.com>
2024-03-18 10:35:12 +0900

    [Error handling] remove space in dockerfile

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,9 @@
 """
 Django Command to wait for database to be available
 """
+from psycopg2 import OperationalError as Psycopg2Error
 
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -9,4 +11,15 @@ class Command(BaseCommand):
     """Django Command to wait for database to be available"""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command."""
+        self.stdout.write("Waiting for database")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Database unvailable, waiting 1 second ....")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available'))
